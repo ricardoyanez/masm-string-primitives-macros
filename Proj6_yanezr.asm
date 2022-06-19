645493c3fa9e4c44a224f69beb1bdfcfc8f68aa1
Ricardo Yanez <yanezr@oregonstate.edu>
2022-06-19 05:29:08 -0700

    macro mGetString and procedure Introduction

--- Proj6_yanezr.asm
+++ Proj6_yanezr.asm
@@ -1,7 +1,7 @@
 TITLE Program Template     (template.asm)
 
 ; Author: Ricardo Yanez
-; Last Modified:
+; Last Modified: 06/16/2022
 ; OSU email address: yanezr@oregonstate.edu
 ; Course number/section:   CS271 Section 404
 ; Project Number: 6        Due Date: 06/26/2022
@@ -10,22 +10,70 @@ TITLE Program Template     (template.asm)
 
 INCLUDE Irvine32.inc
 
-; (insert macro definitions here)
+MAXNUM = 3
 
-; (insert constant definitions here)
+; A 32-bit sign integer has at most 10 decimal digits, add one 
+; for the sign, add one for NULL termination of a string
+MAXSIZE = 12
+
+mGetString MACRO text, count, string
+  LOCAL prompt
+  .data
+  prompt BYTE text, 0
+  .code
+  PUSH EDX
+  PUSH ECX
+  ; display prompt
+  MOV EDX, OFFSET prompt
+  CALL WriteString
+  ; read user-supplied string
+  MOV EDX, OFFSET string
+  MOV ECX, SIZEOF string
+  CALL ReadString
+  MOV count, EAX
+  POP ECX
+  POP EDX
+ENDM
 
 .data
 
-; (insert variable definitions here)
+  greeting		BYTE	"PROGRAMMING ASSIGNMENT 6: Designing low-level I/O procedures",10,13,
+						"Written by: Ricardo Yanez",0
+  prompt1		BYTE	"Please provide 10 signed decimal integers.",10,13,
+						"Each number needs to be small enough to fit inside a 32 bit register. ",
+						"After you have finished inputting the raw numbers I will display a list ",
+						"of the integers, their sum, and their average value.",0
 
 .code
 main PROC
 
-; (insert executable instructions here)
+  PUSH OFFSET prompt1
+  PUSH OFFSET greeting
+  CALL introduction
 
-	Invoke ExitProcess,0	; exit to operating system
+  Invoke ExitProcess,0	; exit to operating system
 main ENDP
 
-; (insert additional procedures here)
+introduction PROC
+  PUSH EBP
+  MOV EBP, ESP
+
+  PUSH EDX
+
+  MOV EDX, [EBP+8]
+  CALL WriteString
+  CALL CrLf
+  CALL CrLf
+
+  MOV EDX, [EBP+12]
+  CALL WriteString
+  CALL CrLf
+  CALL CrLf
+
+  POP EDX
+
+  POP EBP
+  RET 8
+introduction ENDP
 
 END main
