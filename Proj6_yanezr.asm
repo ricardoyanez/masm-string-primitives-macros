5f2126a991db9e6970ed583057953d12b537f16b
Ricardo Yanez <yanezr@oregonstate.edu>
2022-06-19 05:23:55 -0700

    first commit

---
+++ Proj6_yanezr.asm
@@ -0,0 +1,31 @@
+TITLE Program Template     (template.asm)
+
+; Author: Ricardo Yanez
+; Last Modified:
+; OSU email address: yanezr@oregonstate.edu
+; Course number/section:   CS271 Section 404
+; Project Number: 6        Due Date: 06/26/2022
+; Description: This file is provided as a template from which you may work
+;              when developing assembly projects in CS271.
+
+INCLUDE Irvine32.inc
+
+; (insert macro definitions here)
+
+; (insert constant definitions here)
+
+.data
+
+; (insert variable definitions here)
+
+.code
+main PROC
+
+; (insert executable instructions here)
+
+	Invoke ExitProcess,0	; exit to operating system
+main ENDP
+
+; (insert additional procedures here)
+
+END main
